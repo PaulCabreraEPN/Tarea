823d9bbe64fc7d5f7ac04d8091c7653df5cd30dc
TADIO1829 <151254512+TADIO1829@users.noreply.github.com>
2024-05-23 11:03:01 -0500

    Modificacion cuadrado y pentagono

--- src/main/java/org/example/Pentagono.java
+++ src/main/java/org/example/Pentagono.java
@@ -1,27 +1,28 @@
 package org.example;
 
-public class Cuadrado extends FigurasGeometricas{
-    Double lado;
+public class Pentagono extends FigurasGeometricas{
+    private double lado;
 
-    public Cuadrado(Double lado) {
-        super("Cuadrado", 4);
+    public Pentagono(double lado) {
+        super("Pentagono", 5);
         this.lado = lado;
     }
 
-
     public double calcularArea() {
-        return lado * lado;
+
+        double apotema = lado / (2 * Math.tan(Math.PI / 5));
+        return (5 * lado * apotema) / 2;
     }
 
     public double calcularPerimetro() {
-        return 4 * lado;
+        return 5 * lado;
     }
+
+    @Override
     public void mostrarInformacion() {
         System.out.println("Nombre: " + getNombre());
         System.out.println("Número de lados: " + getNumero_lados());
         System.out.println("Área: " + calcularArea());
         System.out.println("Perímetro: " + calcularPerimetro());
     }
-
-
 }
